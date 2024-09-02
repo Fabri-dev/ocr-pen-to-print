157e1b1fa82dd3ffd8ba73fae8d4efe7a2655c39
Fabrizio Golisciano <72052696+Fabri-dev@users.noreply.github.com>
2024-09-01 22:33:52 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,23 +5,13 @@ const $clearButton= document.getElementById("clearButton");
 const $inputSection= document.getElementById("inputSection");
 const $dropHere= document.getElementById("dropHere");
 const $outputText= document.getElementById("outputText");
-
+const $buttons= document.getElementById("buttons");
+const $main = document.querySelector("main");
+const $inputAndImage= document.getElementById("inputAndImage");
 const apiUrl = 'https://pen-to-print-handwriting-ocr.p.rapidapi.com/recognize/';
 
 
 
-$userForm.addEventListener("submit", async (event)=>{
-    event.preventDefault(); //cancela que intente utilizar el metodo action
-    submitButton.setAttribute("disabled",''); // Deshabilita el botón mientras carga asi no se pueden hacer multiples llamados
-
-    //cargamos la informacion que utilizaremos para hacer el llamado al API
-
-    const file = $userFile.files[0]; //leemos y retornamos el archivo recibido
-
-    const options= loadOptionsForApi(file);
-
-    callToApi(options);
-});
 
 const callToApi= async options =>{
     try {
@@ -45,7 +35,7 @@ const loadOptionsForApi= file =>{
         options = {
             method: 'POST', // por default si no escribimos explicitamente method, se hara GET.
             headers: {
-                'x-rapidapi-key': 'APIKEY',
+                'x-rapidapi-key': '594c5b388amshaaee958da1dcb18p1093f9jsn8ed63e3fd285',
                 'x-rapidapi-host': 'pen-to-print-handwriting-ocr.p.rapidapi.com'
             },
             body: data
@@ -55,11 +45,26 @@ const loadOptionsForApi= file =>{
     return options;
 }
 
+$userForm.addEventListener("submit", async (event)=>{
+    event.preventDefault(); //cancela que intente utilizar el metodo action
+    submitButton.setAttribute("disabled",''); // Deshabilita el botón mientras carga asi no se pueden hacer multiples llamados
+
+    //cargamos la informacion que utilizaremos para hacer el llamado al API
+
+    const file = $userFile.files[0]; //leemos y retornamos el archivo recibido
+
+    const options= loadOptionsForApi(file);
+
+    callToApi(options);
+});
+
 let lastSelectedFile= null;
 
 $userFile.addEventListener("change",()=>{
     if ($userFile.files.length > 0) {
         lastSelectedFile = $userFile.files[0]; // Guarda el archivo seleccionado
+        createImage(lastSelectedFile);
+
     }else{
         // Si el input file está vacío pero hay un archivo guardado, restaurarlo, esto sucede cuando cierro el selector de archivos sin haber seleccionado nada, me retorna null y me borra el archivo que habia seleccionado anteriormente
         if(lastSelectedFile){
@@ -86,54 +91,84 @@ $clearButton.addEventListener("click",()=>{
 
 });
 
-$inputSection.addEventListener("dragover",(event)=>{
+let dragCounter = 0; // Contador de eventos drag
+
+$main.addEventListener("dragenter",(event)=>{
     event.preventDefault();
-    $inputSection.style.backgroundColor= "lightgray";
-
-
+    //verifico si lo arrastrado es un archivo
+    if(event.dataTransfer.types.includes('Files')){
+        dragCounter++;
+        $dropHere.classList.replace('inactive','active');
+    }
 });
 
-$inputSection.addEventListener("dragleave",(event)=>{
-    event.preventDefault();
-    $inputSection.style.backgroundColor= "";
+// Manejador para cuando un archivo está sobre el área de drop
+$main.addEventListener('dragover', (event) => {
+    event.preventDefault(); // Previene la acción predeterminada
 });
 
-$inputSection.addEventListener("drop",(event)=>{
+$main.addEventListener("dragleave",(event)=>{
+    event.preventDefault();
+    dragCounter--;
+    if(dragCounter ===0){
+        $dropHere.classList.replace('active','inactive');
+    }
+});
+
+
+
+$main.addEventListener("drop",(event)=>{
     event.preventDefault();
 
     const files= event.dataTransfer.files;
 
     //console.log(files);
-
-    //no se puede enviar mas de un archivo
-    if(files.length === 1)
-    {
-        const dataTransfer = new DataTransfer(); //sirve para transferir datos/bytes
-
-        const oneFile= files.item(0);
-
-        dataTransfer.items.add(oneFile);//agrego un archivo al objeto dataTransfer
-        $userFile.files = dataTransfer.files; //transfiero el archivo al input desde javascript
+    
+    //verifico que sea un dato drageado de tipo file, asi me activo
+    if(event.dataTransfer.types.includes('Files')){
         
-        const fileImage=document.createElement("img"); //creo una etiqueta de img 
+        //no se puede enviar mas de un archivo
+        if(files.length === 1)
+        {
+            const dataTransfer = new DataTransfer(); //sirve para transferir datos/bytes
+            const oneFile= files.item(0);
 
-        fileImage.src=returnUrlImage(oneFile); //retorno el archivo como url
-        fileImage.id="fileImage";
-        fileImage.alt=oneFile.name; 
+            dragCounter = 0; // Reiniciamos el contador
+            $dropHere.classList.replace('active','inactive');//desactivo el drophere!
+            
+            //$main.style.backgroundColor= ""; //cambio el fondo
 
-        //fileImage.classList.addd("INGRESAR ESTILo");
-
-        $inputSection.append(fileImage); //agrego la etiqueta creada al final
-
-
-        $inputSection.style.backgroundColor= "yellow";
-        
-    }else{
-        $outputText.textContent="You can not upload more than 1 file";
+            dataTransfer.items.add(oneFile);//agrego un archivo al objeto dataTransfer
+            $userFile.files = dataTransfer.files; //transfiero el archivo al input desde javascript
+            
+            createImage(oneFile); //muestro la imagen             
+        }else{
+            $outputText.textContent="You can not upload more than 1 file";
+        }
     }
+
 
 });
 
 const returnUrlImage= (oneFile)=>{
     return URL.createObjectURL(oneFile); // creo el archivo como una URL 
 };
+
+const createImage= (oneFile) =>{
+    
+    //si hay una imagen la elimino y creo la otra
+    if(document.getElementById("fileImage")){
+        document.getElementById("fileImage").remove();
+    }
+
+    let fileImage=document.createElement("img"); //creo una etiqueta de img 
+    
+    fileImage.src=returnUrlImage(oneFile); //retorno el archivo como url
+    fileImage.id="fileImage";
+    fileImage.alt=oneFile.name; 
+    $inputAndImage.append(fileImage); //agrego la etiqueta creada al final
+}
+
+
+
+
